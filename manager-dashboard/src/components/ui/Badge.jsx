92a2651dec453459b92d3f8dea853c49858ec1c1
shravyavenishetty <shravyavenishetty03@gmail.com>
2026-02-03 10:39:03 +0530

    deleted unwanted files

--- manager-dashboard/src/components/ui/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import { getStatusBadgeClass } from '../../lib/utils';
-
-export const Badge = ({ status, children, className = '' }) => {
-    if (status) {
-        return <span className={getStatusBadgeClass(status)}>{children || status}</span>;
-    }
-
-    return <span className={className}>{children}</span>;
-};
